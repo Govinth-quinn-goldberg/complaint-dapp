6fcd1be9b10937f7d305503235eeaaefa39683e8
Govinth-quinn-goldberg <govinth.v20@gmail.com>
2025-10-28 11:50:22 +0530

    Removed private key from config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
-require("@nomiclabs/hardhat-ethers"); // ensure plugin installed
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-const { PRIVATE_KEY, RPC_URL } = process.env;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const RPC_URL = process.env.RPC_URL;
 
 module.exports = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: RPC_URL || "http://127.0.0.1:8545/",
-      accounts: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 ? [0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266] : []
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY]
     }
   }
 };
